abf6d5275fedbd052a95eeb9a32c2433783ca7db
apple <apple@zty.local>
2019-11-04 23:25:55 +0000

    complete

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class Main {
         double meanr = 0;
         double varf = 0;
         double varr = 0;
-        Cache fifo = new FIFOCache(20, 10,  1000L * 1000 * 1000);
-        Cache random = new RandomCache(20, 10,  1000L * 1000 * 1000);
+        int dataSize = 1000;
+        int cacheSize = 100;
+        System.out.println("M = " + dataSize + " N = " + cacheSize);
+        Cache fifo = new FIFOCache(dataSize, cacheSize,  1000L * 1000 * 1000);
+        Cache random = new RandomCache(dataSize, cacheSize,  1000L * 1000 * 1000);
         for(int i = 0; i < TIME; i++) {
             rs[i] = random.simulate();
             fs[i] = fifo.simulate();
@@ -39,4 +42,31 @@ public class Main {
         System.out.println("mean : " + mean);
         System.out.println("CI : " + (mean - half) + " - " + (mean + half));
     }
+
+    //RESULTS
+    /**
+     * M = 1000 N = 10
+     * FIFO :
+     * mean : 0.8176683678352712
+     * CI : 0.8173329347302327 - 0.8180038009403097
+     * RANDOM :
+     * mean : 0.8177016408261839
+     * CI : 0.8173607260155625 - 0.8180425556368053
+     *
+     * M = 1000 N = 50
+     * FIFO :
+     * mean : 0.5942907044154555
+     * CI : 0.5938394776825688 - 0.5947419311483422
+     * RANDOM :
+     * mean : 0.5942677563992559
+     * CI : 0.5938145796858779 - 0.5947209331126339
+     *
+     * M = 1000 N = 100
+     * FIFO :
+     * mean : 0.47900155146115325
+     * CI : 0.4785317632596019 - 0.4794713396627046
+     * RANDOM :
+     * mean : 0.47914904642106276
+     * CI : 0.4786072388161022 - 0.47969085402602335
+     */
 }
